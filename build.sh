79c8974fe2a578cd92b5eb03d4eba8e89530ed45
Tony <yangxiangjiang19880805@gmail.com>
2022-10-02 16:55:38 +0800

    up

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 # Get flutter
 # git clone https://github.com/flutter/flutter.git
 FLUTTER=flutter/bin/flutter
+
 # Configure flutter
-# $FLUTTER channel beta
-# $FLUTTER upgrade
+$FLUTTER channel beta
+$FLUTTER upgrade
 $FLUTTER config --enable-web
+# build web
 $FLUTTER build web --release
 
 echo "Build Web Application Success!!!"
